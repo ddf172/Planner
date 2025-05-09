7fad6a77d4eaa9b89290d5a100b34f53f7bad479
ddf172 <jakubddfadamski@gmail.com>
2025-05-10 01:50:39 +0200

    test1

---
+++ client/workspace/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+
+int main(){
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    const char* message = "TEST TEST TEST";
+    send(clientSocket, message, strlen(message), 0);
+
+    return 0;
+}
